c37b6ad871a9abff7e79ce28d18bfda48195204c
Wenxun Wu <wenxunwu95@gmail.com>
2025-03-18 16:08:53 +0800

    [feat] add configuration and scripts for GRPO training on shale oil dataset

--- examples/run_qwen2_5_7b_so.sh
+++ examples/run_qwen2_5_7b_so.sh
@@ -8,22 +8,18 @@ export VLLM_ATTENTION_BACKEND=XFORMERS
 export VLLM_USE_V1=0
 
 SYSTEM_PROMPT="""You FIRST think about the reasoning process as an internal monologue and then provide the final answer.
- The reasoning process MUST BE enclosed within <think> </think> tags. The final answer MUST BE put in \boxed{} if it is a math problem."""
+ The reasoning process MUST BE enclosed within <think> </think> tags. The final answer MUST BE put in \boxed{}"""
 
 python3 -m verl.trainer.main \
-    config=examples/grpo_example.yaml \
+    config=examples/grpo_shale_oil.yaml \
     data.system_prompt="${SYSTEM_PROMPT}" \
-    data.train_files=${TRAIN_FILES:-hiyouga/math12k@train} \
-    data.val_files=${VAL_FILES:-hiyouga/math12k@test} \
-    data.max_prompt_length=${MPL:-2048} \
-    data.max_response_length=${MRL:-2048} \
+    data.train_files=${TRAIN_FILES:-/mnt/zj-gpfs/home/wuwx/EasyR1/ShaleOil3000/train.parquet} \
+    data.val_files=${VAL_FILES:-/mnt/zj-gpfs/home/wuwx/EasyR1/ShaleOil3000/test.parquet} \
     worker.actor.model.model_path=${MODEL_DIR:-/mnt/models/models/public/Qwen/Qwen2.5/Qwen2.5-7B-Instruct/} \
-    worker.rollout.max_num_batched_tokens=${MNBT:-8192} \
-    trainer.total_episodes=${TE:-30} \
     trainer.logger=['console','swanlab'] \
-    trainer.experiment_name=${EXPERIMENT:-qwen2_5_7b_math} \
+    trainer.experiment_name=${EXPERIMENT:-qwen2_5_7b_shaleoil} \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=${NNODES:-1} \
-    trainer.save_freq=${SAVE_FREQ:-50} \
+    trainer.save_freq=${SAVE_FREQ:-20} \
     trainer.save_checkpoint_path=${OUTPUT_DIR:-/mnt/train/home/wuwx/easyr1_results} \
     trainer.load_checkpoint_path=${LOAD_CHECKPOINT_PATH:-null} \
